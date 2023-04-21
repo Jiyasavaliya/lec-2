e8de0726e9fce661071b46d411491f72e1a57e87
jiyasavaliya18 <126229948+jiyasavaliya18@users.noreply.github.com>
2023-04-22 00:28:16 +0530

    Update static.cpp

--- static.cpp
+++ static.cpp
@@ -1,6 +1,4 @@
-#include <iostream>
-
-
+#include<iostream>
 using namespace std;
 
 class Hotel {
@@ -11,28 +9,28 @@ private:
     char hotel_rating;
     char hotel_location;
     int hotel_establish_year;
-	int hotel_staff;
+	int hotel_staff_quantity;
    	char hotel_room_quantity;
 public:
-    void setHotel(int id, char name, char type, char rating, char location, int establish_year, int staff_quantity, char room_quantity) {
+    void  hotel(int id, char name, char type, char rating, char location, int establish_year, int staff_quantity, char room_quantity) {
         hotel_id = id;
         hotel_name = name;
         hotel_type = type;
         hotel_rating = rating;
         hotel_location = location;
-        hotel_establish_year = establish_year_quantity;
-	    hotel_staff = staff;
+        hotel_establish_year = establish_year;
+	    hotel_staff_quantity = staff_quantity;
    	    hotel_room_quantity = hotel_room_quantity;    
     }
-    void displayHotel() {
+    void hotel() {
         cout << "Hotel ID: " << hotel_id << endl;
         cout << "Hotel Name: " << hotel_name << endl;
         cout << "Hotel Type: " << hotel_type << endl;
         cout << "Hotel rating: " << hotel_rating << endl;
         cout << "Hotel location: " << hotel_location << endl;
-        cout << "Hotel establish_year_quantity: " << hotel_establish_year_quantity << endl;
-        cout << "Hotel staff : " << hotel_staff << endl;
-        cout << "Hotel hotel_room_quantity: " << hotel_hotel_room_quantity << endl;
+        cout << "Hotel establish_year_quantity: " << hotel_establish_year << endl;
+        cout << "Hotel staff : " << hotel_staff_quantity << endl;
+        cout << "Hotel hotel_room_quantity: " << hotel_room_quantity << endl;
         
     }
 };
@@ -46,7 +44,7 @@ int main() {
 
     for (int i = 0; i < n; i++) {
         int id;
-        string name, type;
+        string name, type, rating, location, establish_year, staff_quantity, room_quantity;
         cout << "Enter hotel ID: ";
         cin >> id;
         cout << "Enter hotel name: ";
@@ -57,12 +55,12 @@ int main() {
         cin >> rating;
 	    cout << "Enter hotel location: ";
         cin >> location;
-	    cout << "Enter hotel establish_year_quantity: ";
-        cin >> establish_year_quantity;
-	    cout << "Enter hotel staff: ";
-        cin >> staff;
-	    cout << "Enter hotel_room_quantity: ";
-        cin >> hotel_room_quantity;
+	    cout << "Enter hotel establish year: ";
+        cin >> establish_year;
+	    cout << "Enter hotel staff quantity: ";
+        cin >> staff_quantity;
+	    cout << "Enter hotel room quantity: ";
+        cin >> room_quantity;
 	
     }
 
